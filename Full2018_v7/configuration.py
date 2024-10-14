4920ae0e677472e2e97338cc25f1b2f81d1d1745
piedraj <piedra@cern.ch>
2024-10-14 11:14:01 +0200

    Update configuration.py

--- Full2018_v7/configuration.py
+++ Full2018_v7/configuration.py
@@ -8,7 +8,7 @@ It's the only necessary python configuration file, all the other files are impor
 treeName = 'Events'
 
 #: tag used to identify the configuration folder version
-tag = 'darkHiggs2018_v7'
+tag = 'TFG'
 
 #: file to use as runner script, default uses mkShapesRDF.shapeAnalysis.runner, otherwise specify path to script
 runnerFile = "default"
