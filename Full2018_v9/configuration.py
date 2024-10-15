104b85e4d64e55f1f4d26c58d61cbc72a5846976
piedraj <piedra@cern.ch>
2024-10-15 12:38:06 +0200

    Add files via upload

--- Full2018_v9/configuration.py
+++ Full2018_v9/configuration.py
@@ -2,12 +2,13 @@
 Configuration file for mkShapesRDF script.
 
 It's the only necessary python configuration file, all the other files are imported and defined by this one.
+
 """
 
 treeName = 'Events'
 
 # Tag used to identify the configuration folder version
-tag = 'v9'
+tag = 'Full2018_v7'
 
 # File to use as runner script, default uses mkShapesRDF.shapeAnalysis.runner, otherwise specify path to script
 runnerFile = "default"
@@ -24,37 +25,35 @@ batchFolder = "condor"
 # Path to configuration folder (will contain all the compiled configuration files)
 configsFolder = "configs"
 
-# Luminosity to normalize to (in 1/fb)
-# https://github.com/latinos/LatinoAnalysis/blob/UL_production/NanoGardener/python/data/TrigMaker_cfg.py#L868 (874)
-#  59.832475339
-lumi = 59.83
+# File with TTree aliases
+aliasesFile = 'aliases.py'
 
-# File with dict of aliases to define
-aliasesFile = "aliases.py"
+# File with list of variables
+variablesFile = 'variables.py'
 
-# File with dict of variables
-variablesFile = "variables.py"
+# File with list of cuts
+cutsFile = 'cuts.py' 
 
-# File with dict of cuts
-cutsFile = "cuts.py"
+# File with list of samples
+samplesFile = 'samples.py' 
 
-# File with dict of samples
-samplesFile = "samples.py"
-
-# File with dict of samples for plotting
+# File with colors of samples
 plotFile = 'plot.py' 
 
-# File with dict of structure (used to define processes in combine)
-structureFile = "structure.py"
+# Luminosity to normalize to (in 1/fb)
+lumi = 59.83
+
+# Used by mkDatacards to define output directory for datacards
+outputDirDatacard = "datacards__{}".format(tag)
+
+# Structure file for datacard
+structureFile = 'structure.py'
 
 # Nuisances file for mkDatacards and for mkShape
-nuisancesFile = "nuisances.py"
+nuisancesFile = 'nuisances.py'
 
 # Path to folder where to save plots
 plotPath = "plots__{}".format(tag)
-
-# This lines are executed right before the runner on the condor node
-mountEOS = []
 
 # List of imports to import when compiling the whole configuration folder, it should not contain imports used by configuration.py
 imports = ["os", "glob", ("collections", "OrderedDict"), "ROOT"]
